a12b0ffcaa9e3b74d88ad4a8d11d3f5314613b6c
Stephen-Kimoi <moenisteve@gmail.com>
2024-05-20 14:28:37 +0300

    first section of events

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5960dbe240c6cd94"; }
+/******/ 		__webpack_require__.h = function() { return "098d15ccc3307548"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
