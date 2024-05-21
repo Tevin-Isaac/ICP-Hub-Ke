abf970d7937d02abc945f721e58117d3918451d3
steve kimoi <87986813+Stephen-Kimoi@users.noreply.github.com>
2024-05-21 12:34:48 +0000

    deleting next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/about":["static\u002Fchunks\u002Fpages\u002Fabout.js"],"/contact":["static\u002Fchunks\u002Fpages\u002Fcontact.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fabout","\u002Fcontact"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/events":["static\u002Fchunks\u002Fpages\u002Fevents.js"],"/project":["static\u002Fchunks\u002Fpages\u002Fproject.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fevents","\u002Fproject"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> refs/remotes/origin/master
