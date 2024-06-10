e97ab89180b7679493b750d659d84f9fb9c45cc8
Stephen-Kimoi <moenisteve@gmail.com>
2024-06-10 18:48:45 +0300

    partners section images

--- components/organisms/LogoList/index.tsx
+++ components/organisms/LogoList/index.tsx
@@ -6,57 +6,28 @@ import SectionSentence from 'components/molecules/SectionSentence'
 const LogoList = () => {
   return (
     <>
-         <SectionSentence
-            badge="OUR PARTNERS"
-           
-            
-          />
-      <section
-      
-        className="py-8 m-0 flex flex-row flex-wrap place-items-center justify-around gap-4 md:gap-0 border-y border-borderLight animate-marquee"
-        data-aos="fade-left"
-      >
-        <div className="animate-marquee-item">
-          <Image
-            src={"/images/github-logo.svg"}
-            width={120}
-            height="36"
-            alt="github logo"
-          />
+      <SectionSentence
+        badge="OUR PARTNERS"
+      />
+      <div className="overflow-hidden border-y border-borderLight">
+        <div className="py-8 animate-marquee whitespace-nowrap">
+
+          <div className="inline-flex space-x-16">
+            {[...Array(34)].map((_, index) => (
+              <div key={index} className="animate-marquee-item">
+                <Image
+                  src={`/images/partner-logos/partner-logo-${index + 1}.png`}
+                  alt={`Partner Logo ${index + 1}`} 
+                  width={150}
+                  height={75}
+                  // objectFit="contain"
+                />
+              </div>
+            ))}
+          </div>
+
         </div>
-        <div className="animate-marquee-item">
-          <Image
-            src={"/images/forbes-logo.svg"}
-            width={120}
-            height="36"
-            alt="forbes logo"
-          />
-        </div>
-        <div className="animate-marquee-item">
-          <Image
-            src={"/images/google-logo.svg"}
-            width={120}
-            height="36"
-            alt="google logo"
-          />
-        </div>
-        <div className="animate-marquee-item">
-          <Image
-            src={"/images/microsoft-logo.svg"}
-            width={120}
-            height="36"
-            alt="microsoft logo"
-          />
-        </div>
-        <div className="animate-marquee-item">
-          <Image
-            src={"/images/facebook-logo.svg"}
-            width={120}
-            height="36"
-            alt="facebook logo"
-          />
-        </div>
-      </section>
+      </div>
     </>
   );
 };
