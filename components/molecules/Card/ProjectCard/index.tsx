7346cd05db0ba5babf40a9abd7fbb3098c378caa
Tevin-Isaac <isaactevin@yahoo.com>
2024-06-12 13:09:40 +0300

    updated project card in project card folder

--- components/molecules/Card/ProjectCard/index.tsx
+++ components/molecules/Card/ProjectCard/index.tsx
@@ -1,52 +1,73 @@
-import Image from 'next/image'
-import Button from 'components/atoms/Button'
-import ButtonLink from 'components/atoms/Button/ButtonLink'
-import Text from 'components/atoms/Text'
+import Image from "next/image";
+import Button from "components/atoms/Button";
+import ButtonLink from "components/atoms/Button/ButtonLink";
+import Text from "components/atoms/Text";
 
 interface ProjectCardProps {
-  title?: string
-  lightTitle?: string
-  description?: string
-  lightDescription?: string
-  imageSource?: string
-  imageAlt?: string
-  height?: number
-  width?: number
+  title?: string;
+  lightTitle?: string;
+  description?: string;
+  lightDescription?: string;
+  imageSource?: string;
+  imageAlt?: string;
+  height?: number;
+  width?: number;
 }
 
 const ProjectCard = ({
-  title = '',
-  lightTitle = '',
-  description = '',
-  lightDescription = '',
+  title = "",
+  lightTitle = "",
+  description = "",
+  lightDescription = "",
   imageSource,
   imageAlt,
-  height = 450,
-  width,
+  height = 180,
+  width = 180,
 }: ProjectCardProps) => {
   return (
     <div className="w-full">
       {imageSource && (
-        <div className="relative w-full overflow-hidden" style={{ height: `${height}px` }}>
-          <Image src={imageSource} alt={imageAlt || ''} layout="fill" objectFit="contain" />
+        <div
+          className="relative w-full overflow-hidden"
+          style={{ height: `${height}px` }}
+        >
+          <Image
+            src={imageSource}
+            alt={imageAlt || ""}
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
       )}
       <div className="flex flex-col place-items-center text-center mt-10">
         <div className="mb-2.5">
-          <Text value={lightTitle || title} textStyle={lightTitle ? 'ProjectTitleLight' : 'ProjectTitle'} />
+          <Text
+            value={lightTitle || title}
+            textStyle={lightTitle ? "ProjectTitleLight" : "ProjectTitle"}
+          />
         </div>
         <div className="mb-9">
           <Text
             value={lightDescription || description}
-            textStyle={lightDescription ? 'ProjectDescriptionLight' : 'ProjectDescription'}
+            textStyle={
+              lightDescription
+                ? "ProjectDescriptionLight"
+                : "ProjectDescription"
+            }
           />
         </div>
         <div className="">
-          <ButtonLink value="Detail" color="#313131" radius="pill" style="outline" href="https://internetcomputer.org/" />
+          <ButtonLink
+            value="Detail"
+            color="#313131"
+            radius="pill"
+            style="outline"
+            href="https://internetcomputer.org/"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
